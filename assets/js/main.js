5aebaa6ffa6e9639045949d777d967ac8c775c04
Daniel Hernandez <danny20031405@gmail.com>
2025-11-23 13:37:40 -0600

    add gallery phothos in galley-secretos.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,16 @@ document.querySelectorAll('.dropdown-toggle').forEach(toggle => {
         menu.style.display = menu.style.display === "flex" ? "none" : "flex";
     });
 });
+
+
+    const links = document.querySelectorAll('#navbar a');
+    const current = window.location.pathname.split("/").pop();
+
+    links.forEach(link => {
+        const href = link.getAttribute('href');
+
+        // Marca el activo según la página actual
+        if (href === current) {
+            link.classList.add('active');
+        }
+    });
